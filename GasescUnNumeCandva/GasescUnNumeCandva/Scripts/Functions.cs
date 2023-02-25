c801252c05fe672d4507d3926293876b3d72e1f7
Cojocaru Cristian <cojocaru.cristian20@gamil.com>
2023-02-25 12:25:38 +0200

    Show result

---
+++ GasescUnNumeCandva/GasescUnNumeCandva/Scripts/Functions.cs
@@ -0,0 +1,126 @@
+﻿namespace GasescUnNumeCandva.Scripts;
+
+public class Functions
+{
+    public static int[] ConvertToBinary(int nr)
+    {
+        int[] array;
+        int[] aux = new int[2];
+        int size = 0;
+
+        while (nr != 0)
+        {
+            if (size == aux.Length)
+            {
+                Console.WriteLine("Am marit dimensiunea!");
+                int[] aux2 = new int[aux.Length * 2];
+                for (int i = 0; i < size; i++)
+                {
+                    aux2[i] = aux[i];
+                }
+
+                aux = aux2;
+            }
+
+            aux[size++] = Int32.Abs(nr % 2);
+            nr = nr / 2;
+        }
+
+        array = new int[size+1];
+        if (nr >= 0)
+        {
+            array[0] = 0;
+        }
+        else
+        {
+            array[0] = 1;
+        }
+
+        for (int i = size - 1; i >= 0; i--)
+        {
+            array[size - i] = aux[i];
+        }
+        
+        Console.WriteLine("binary number: " + array);
+        
+        return array;
+    }
+    
+    public static int[] MakeArray(int nr)
+    {   
+        int[] array = new int[2];
+        int size = 0;
+
+        while (nr != 0)
+        {
+            if (size == array.Length)
+            {
+                int[] aux = new int[array.Length * 2];
+                for (int i = 0; i < size; i++)
+                {
+                    aux[i] = array[i];
+                }
+
+                array = aux;
+            }
+
+            array[size++] = nr % 10;
+            nr = nr / 10;
+        }
+
+        return array;
+    }
+
+    public static void Rshift(int[] A, int[] Q)
+    {
+        for(int i = Q.Length - 1; i > 0; i--) {
+            Q[i] = Q[i-1];
+        }
+        Q[0] = A[A.Length-1];
+        for (int i = A.Length - 1; i > 0; i--)
+        {
+            A[i] = A[i - 1];
+        }
+        
+    }
+
+    public static void Add(int[] A, int[] M)
+    {
+        int aux = 0;
+        int c_in = 0;
+        for(int i = A.Length-1; i > 0; i--) {
+            aux = (A[i] + M[i] + c_in);
+            A[i] = aux%2;
+            if(aux >= 2) {
+                c_in = 1;
+            } else {
+                c_in = 0;
+            }
+        }
+        A[0] = c_in;
+    }
+
+    public static String Afiseaza(int[] A, int[] Q, int[] M)
+    {
+        String str = "";
+        //"A\t\t\tQ\t\t\tM\n";
+        for (int i = 0; i < A.Length; i++)
+        {
+            str += A[i];
+        }
+        str += "\t\t\t";
+        for (int i = 0; i < Q.Length; i++)
+        {
+            str += Q[i];
+        }
+        /*
+         * Console.Out.Write("\t\t\t");
+        for (int i = 0; i < M.Length; i++)
+        {
+            str += M[i];
+        }
+
+         */
+        return str;
+    }
+}
---
+++ GasescUnNumeCandva/GasescUnNumeCandva/Scripts/SignMagnitude.cs
@@ -0,0 +1,72 @@
+﻿namespace GasescUnNumeCandva.Scripts;
+
+public class SignMagnitude
+{
+    private int number_type;
+    private int format_type;
+    private int nr1, nr2;
+    private int count = 0;
+    //private Functions _functions;
+
+    public SignMagnitude(int nr1, int nr2, int numberType, int formatType)
+    {
+        this.nr1 = nr1;
+        this.nr2 = nr2;
+        this.number_type = numberType;
+        this.format_type = formatType;
+    }
+    
+    public String Calculate()
+    {
+        int[] A = new int[10];
+        int[] Q = new int[10];
+        int[] M = new int[10];
+        if (format_type == 1)
+        {   
+            A = Functions.MakeArray(nr2);
+            //Console.WriteLine(A);
+            Q = Functions.MakeArray(nr1);
+            //Console.WriteLine(A);
+            M = Functions.MakeArray(nr2);
+            //Console.WriteLine(A);
+        }
+        else 
+        {
+            A = Functions.ConvertToBinary(nr2);
+            Q = Functions.ConvertToBinary(nr1);
+            M = Functions.ConvertToBinary(nr2);
+        }
+        
+        while (count < Q.Length - 1)
+        {
+            if (Q[Q.Length - 1] == 1)
+            {
+                Functions.Add(A, M);
+            }
+            
+            Functions.Rshift(A, Q);
+            A[0] = 0;
+            count++;
+        }
+        
+        int aux1 = Q[Q.Length-1];
+        int aux2 = M[0];
+
+        if (number_type == 1)
+        {
+            Functions.Rshift(A, Q);
+        }
+        else
+        {
+            Q[Q.Length - 1] = 0;
+        }
+        
+        if((aux1 == 0 && aux2 == 0) || (aux1 == 1 && aux2 == 1)) {
+            A[0] = 0;
+        } else {
+            A[0] = 1;
+        }
+        
+        return Functions.Afiseaza(A, Q, M);
+    }
+}
